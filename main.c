2592037821c88ed6da214d414a20c7e62c082e0b
Alexandre Bustico <alexandre.bustico@enac.fr>
2018-03-07 23:16:19 +0100

    memory to memory working

--- main.c
+++ main.c
@@ -9,8 +9,6 @@
 #include "globalVar.h"
 #include "stdutil.h"
 #include "ttyConsole.h"
-#include "potentiometre.h"
-#include "pwm.h"
 #include "hal_dma.h"
 
 
@@ -26,48 +24,25 @@
  */
 
 
-#define ICU1_CH1_DMA_CONTROLER		2
-#define ICU1_CH1_DMA_STREAM		6
-#define ICU1_CH1_DMA_IRQ_PRIORITY	6
-#define ICU1_CH1_DMA_PRIORITY		2
-#define ICU1_CH1_DMA_CHANNEL		0
-#define ICU1_CHANNEL		ICU_CHANNEL_1
-#define DCR_DBL			(1 << 8) // 2 transfert
-// first register to get is CCR1
-#define DCR_DBA			(((uint32_t *) (&ICUD1.tim->CCR) - ((uint32_t *) ICUD1.tim))) 
+#define M2M_DMA_CONTROLER		2
+#define M2M_DMA_STREAM		6
+#define M2M_DMA_IRQ_PRIORITY	6
+#define M2M_DMA_PRIORITY		2
+#define M2M_DMA_CHANNEL		0
 
-static void initDma(void);
-static bool startDma(void);
-static void stopDma(void);
-static void startDmaAcquisition(uint16_t *widthOrPeriod,
-				size_t depth);
-static void oneShotDmaAcquisition(uint16_t *widthsAndPeriods,
-				  const size_t depth);
-
-static void error_cb(DMADriver *dmap, dmaerrormask_t err);
-static void end_cb(DMADriver *dmap, void *buffer, const size_t num);
-
-static volatile dmaerrormask_t last_err = 0;
-static volatile uint16_t *last_half_buffer = NULL;
-static volatile size_t last_num = 0;
-static volatile size_t cb_count = 0;
 
 static const DMAConfig dmaConfig = {
-  .controller = ICU1_CH1_DMA_CONTROLER,
-  .stream = ICU1_CH1_DMA_STREAM,
-  .channel = ICU1_CH1_DMA_CHANNEL,
-  .dma_priority =  ICU1_CH1_DMA_PRIORITY,
-  .irq_priority = ICU1_CH1_DMA_IRQ_PRIORITY,
-  //.periph_addr = &ICUD1.tim->DMAR, : not a constant, should have to use cmsis definition
-  .periph_addr = &TIM1->DMAR,
-  .direction = DMA_DIR_P2M,
-  .psize = 2,
-  .msize = 2,
-  .inc_peripheral_addr = false,
+  .controller = M2M_DMA_CONTROLER,
+  .stream = M2M_DMA_STREAM,
+  .channel = M2M_DMA_CHANNEL,
+  .dma_priority =  M2M_DMA_PRIORITY,
+  .irq_priority = M2M_DMA_IRQ_PRIORITY,
+  .direction = DMA_DIR_M2M,
+  .psize = 4,
+  .msize = 4,
+  .inc_peripheral_addr = true,
   .inc_memory_addr = true,
   .circular = false,
-  .error_cb = &error_cb,
-  .end_cb = &end_cb,
   .pburst = 0,
   .mburst = 0,
   .fifo = 0
@@ -75,26 +50,11 @@ static const DMAConfig dmaConfig = {
 
 static DMADriver dmap;
 
-static const ICUConfig icu1ch1_cfg = {
-  .mode = ICU_INPUT_ACTIVE_HIGH,
-  .frequency = 1000000,                                    /* 1Mhz ICU clock frequency.   */
-  .width_cb = NULL,
-  .period_cb = NULL,
-  .overflow_cb = NULL,
-  .channel = ICU1_CHANNEL,
-  .dier = TIM_DIER_CC1DE | TIM_DIER_TDE
-};
-
-
-
-
 static THD_WORKING_AREA(waBlinker, 512);
 static noreturn void blinker (void *arg);
 
-static THD_WORKING_AREA(waSyncDmaTest, 512);
-static noreturn void syncDmaTest (void *arg);
-
-static uint16_t samples[128] __attribute__((aligned(4))) = {0}; // took 0.0128 seconds to fill
+static uint32_t bufferFrom[1024];
+static uint32_t bufferTo[1024];
 
 int main(void) {
 
@@ -109,21 +69,17 @@ int main(void) {
   halInit();
   chSysInit();
   initHeap();
-  initDma();
+  dmaObjectInit(&dmap);
+  dmaStart(&dmap, &dmaConfig);
+
+
+  for (size_t i=0; i<ARRAY_LEN(bufferFrom); i++) {
+    bufferFrom[i]=i;
+  }
   
   consoleInit();
 
-  initPotentiometre();
-
-  launchPwm(10000, 100);
-  
   chThdCreateStatic(waBlinker, sizeof(waBlinker), NORMALPRIO, blinker, NULL);
-  startDma();
-  icuStart(&ICUD1, &icu1ch1_cfg);
-  ICUD1.tim->DCR = DCR_DBL | DCR_DBA;
-  icuStartCapture(&ICUD1);
-  chThdCreateStatic(waSyncDmaTest, sizeof(waSyncDmaTest), NORMALPRIO, syncDmaTest, NULL);
-  //icuEnableNotifications(&ICUD1);
   
   consoleLaunch();  
   chThdSleep(TIME_INFINITE); 
@@ -136,76 +92,15 @@ static noreturn void blinker (void *arg)
   (void)arg;
   chRegSetThreadName("blinker");
   while (true) {
-    for (size_t i=0; i< 2; i++) {
-      DebugTrace ("samples[%u] = %u err=%u cnt=%u", i, samples[i], last_err, cb_count);
+    dmaMtransfert(&dmap, bufferFrom, bufferTo, ARRAY_LEN(bufferFrom));
+    for (size_t i=0; i<ARRAY_LEN(bufferTo); i+=50) {
+      DebugTrace ("To[%u] = %lu", i, bufferTo[i]);
     }
-    for (size_t i=ARRAY_LEN(samples)-2; i<ARRAY_LEN(samples) ; i++) {
-      if (last_num) {
-	DebugTrace ("samples[%ul] = %u half_buffer=%p half_index=%u last_num=%u", i, samples[i],
-		    last_half_buffer, last_half_buffer-samples, last_num);
-      } else {
-	DebugTrace ("samples[%ul] = %u , Cb not called", i, samples[i]);
-      }
-    }
+
+    palToggleLine(LINE_C00_LED1);
     chThdSleepMilliseconds(500);
   }
 }
 
-static noreturn void syncDmaTest (void *arg)
-{
-
-  (void)arg;
-  chRegSetThreadName("syncDmaTest");
-  while (true) {
-    palToggleLine(LINE_C00_LED1); 	
-    oneShotDmaAcquisition(samples, ARRAY_LEN(samples));
-    chThdSleepMilliseconds(1000);
-  }
-}
 
 
-static void initDma(void)
-{
-   dmaObjectInit(&dmap);
-}
-
-static bool startDma(void)
-{
-   return dmaStart(&dmap, &dmaConfig);
-}
-
-static void stopDma(void)
-{
-   dmaStop(&dmap);
-}
-
-static void startDmaAcquisition(uint16_t *widthsAndPeriods,
-				const size_t depth)
-{
-  dmaStartPtransfert(&dmap, widthsAndPeriods, depth);
-}
-
-static void oneShotDmaAcquisition(uint16_t *widthsAndPeriods,
-				  const size_t depth)
-{
-  dmaPtransfert(&dmap, widthsAndPeriods, depth);
-}
-
-
-
-static void error_cb(DMADriver *_dmap, dmaerrormask_t err)
-{
-  (void) _dmap;
-  last_err = err;
-}
-
-static void end_cb(DMADriver *_dmap, void *buffer, size_t num)
-{
-  (void) _dmap;
-
-  if (buffer != samples)
-    last_half_buffer = (uint16_t *) buffer;
-
-  last_num = num;
-  cb_count++;
-}
