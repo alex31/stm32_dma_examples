f051d2949df4036d9bf955a9b470d7f259ca08c7
Alexandre Bustico <alexandre.bustico@enac.fr>
2018-03-09 13:49:04 +0100

    compile, not working

--- main.c
+++ main.c
@@ -10,23 +10,37 @@
 #include "stdutil.h"
 #include "ttyConsole.h"
 #include "hal_dma.h"
+#include "bitband.h"
 
 
 /*
 
-  ° connecter A04 (ADC1_IN4) sur le potentiomètre à bouton
-  ° laisser le jumper entre +3.3V et TOPs pour ce potentiomètre
-  ° connecter A15 sur led8 
-  ° connecter A15 sur A08 : rebouclage PWM vers ICU
+  ° alimenter DHT22
+  ° connecter DATA DHT22 sur A08 
   ° connecter B06 (uart1_tx) sur ftdi rx
   ° connecter B07 (uart1_rx) sur ftdi tx
   ° connecter C00 sur led1 
+
  */
 
+/*===========================================================================*/
+/* DHT11 related defines                                                     */
+/*===========================================================================*/
+/*
+ * Width are in useconds
+ */
+#define    MCU_REQUEST_WIDTH                     18000
+#define    DHT_ERROR_WIDTH                         200
+#define    DHT_START_BIT_WIDTH                      80
+#define    DHT_INTER_BIT_WIDTH                      50
 
+/*===========================================================================*/
+/* ICU related code                                                          */
+/*===========================================================================*/
 
-#define ICU1_CHANNEL		ICU_CHANNEL_1
-#define DCR_DBL			(1 << 8) // 2 transfert
+#define    ICU_TIM_FREQ         1000000
+#define    ICU1_CHANNEL		ICU_CHANNEL_1
+#define    DCR_DBL		(1 << 8) // 2 transfert
 // first register to get is CCR1
 #define DCR_DBA			(((uint32_t *) (&ICUD1.tim->CCR) - ((uint32_t *) ICUD1.tim))) 
 
@@ -35,9 +49,16 @@ static void error_cb(DMADriver *dmap, dmaerrormask_t err);
 static void end_cb(DMADriver *dmap, void *buffer, const size_t num);
 
 static volatile dmaerrormask_t last_err = 0;
-static volatile uint16_t *last_half_buffer = NULL;
-static volatile size_t last_num = 0;
-static volatile size_t cb_count = 0;
+static volatile size_t	       cb_count = 0;
+
+typedef union {
+  struct {
+    uint16_t humidity;
+    uint16_t temp;
+    uint8_t  chksum;
+  } __attribute__ ((scalar_storage_order ("big-endian"))) ;
+    uint8_t raw [5];
+}   DhtData;
 
 static const DMAConfig dmaConfig = {
   .stream = STM32_ICU1_CH1_DMA_STREAM,
@@ -47,8 +68,8 @@ static const DMAConfig dmaConfig = {
   //.periph_addr = &ICUD1.tim->DMAR, : not a constant, should have to use cmsis definition
   //  .periph_addr = &TIM1->DMAR,
   .direction = DMA_DIR_P2M,
-  .psize = 2,
-  .msize = 2,
+  .psize = 4,
+  .msize = 4,
   .inc_peripheral_addr = false,
   .inc_memory_addr = true,
   .circular = false,
@@ -63,7 +84,7 @@ static DMADriver dmap;
 
 static const ICUConfig icu1ch1_cfg = {
   .mode = ICU_INPUT_ACTIVE_HIGH,
-  .frequency = 1000000,                                    /* 1Mhz ICU clock frequency.   */
+  .frequency = ICU_TIM_FREQ,
   .width_cb = NULL,
   .period_cb = NULL,
   .overflow_cb = NULL,
@@ -79,8 +100,6 @@ static noreturn void blinker (void *arg);
 
 static THD_WORKING_AREA(waSyncDmaTest, 512);
 static noreturn void syncDmaTest (void *arg);
-
-static uint16_t widthAndPeriods[48] __attribute__((aligned(4))) = {0}; // took 0.0128 seconds to fill
 
 int main(void) {
 
@@ -124,20 +143,71 @@ static noreturn void blinker (void *arg)
 
 static noreturn void syncDmaTest (void *arg)
 {
-
   (void)arg;
   chRegSetThreadName("syncDmaTest");
-  while (true) {
-    msg_t status = dmaTransfertTimeout(&dmap, &TIM1->DMAR, widthAndPeriods, ARRAY_LEN(widthAndPeriods),
-				       TIME_MS2I(100));
-     if (status != MSG_OK) {
-       DebugTrace ("DMA Time OUT");
-     } else {
-     }
 
-     chThdSleepMilliseconds(1000);
+  DhtData dhtData;
+  icucnt_t widthAndPeriods[84] __attribute__((aligned(4))) = {0xff}; // took 0.0128 seconds to fill
+
+
+  while (true) {
+    palSetLineMode(LINE_A08_ICU_IN, PAL_MODE_OUTPUT_PUSHPULL);
+    palClearLine(LINE_A08_ICU_IN);
+    chThdSleepMicroseconds(MCU_REQUEST_WIDTH);
+    palSetLine(LINE_A08_ICU_IN);
+    
+    
+    /*
+     * Initializes the ICU driver 1.
+     * GPIOA8 is the ICU input.
+     */
+    palSetLineMode(LINE_A08_ICU_IN, PAL_MODE_ALTERNATE(AF_LINE_A08_ICU_IN)
+		   | PAL_STM32_PUPDR_PULLUP);
+    chThdSleepMilliseconds(5);
+
+    cb_count =0;
+    msg_t status = dmaTransfertTimeout(&dmap, &TIM1->DMAR, widthAndPeriods, ARRAY_LEN(widthAndPeriods),
+				       TIME_MS2I(1000));
+    if (status != MSG_OK) {
+      DebugTrace ("DMA Time OUT");
+    }
+
+    DebugTrace ("cb_count = %d", cb_count);
+    //chprintf(chp, "\033[2J\033[1;1H");
+
+    // decodage trame binaire DHT
+
+    uint8_t bit_counter=0;
+    volatile uint32_t * const fhtRegBB = bb_sramp(&dhtData, 0);
+    for (size_t i=0; i<ARRAY_LEN(widthAndPeriods)/2; i++) {
+      const icucnt_t width = widthAndPeriods[1+(bit_counter*2)];
+      DebugTrace ("width[%u] = %lu", bit_counter, width);
+      if (width >= DHT_START_BIT_WIDTH)
+	/* starting bit resetting the bit counter */
+	bit_counter = 0;
+      else 
+	fhtRegBB[bit_counter++] = width >= DHT_INTER_BIT_WIDTH;
+    }
   }
+  
+  // decodage champs DHT
+  for (uint i=0; i< sizeof(dhtData.raw); i++) {
+    dhtData.raw[i] = revbit(dhtData.raw[i]);
+  }
+  
+  const float humidity = dhtData.humidity / 10.0f;
+  const float temp = (dhtData.temp & 0x7fff) / ((dhtData.temp & 0x8000) ? - 10.0f : 10.0f);
+  
+  if (dhtData.raw[4] == ((dhtData.raw[0]+dhtData.raw[1]+dhtData.raw[2]+dhtData.raw[3]) & 0xff)) {
+    chprintf(chp, "Temperature: %.2f C, Humidity Rate: %.2f %% \n\r",
+	     temp, humidity);
+  } else {
+    chprintf(chp, "Checksum FAILED!\n\r");
+  }
+  
+  chThdSleepMilliseconds(1000);
 }
+
 
 
 
@@ -150,10 +220,8 @@ static void error_cb(DMADriver *_dmap, dmaerrormask_t err)
 static void end_cb(DMADriver *_dmap, void *buffer, size_t num)
 {
   (void) _dmap;
-
-  if (buffer != widthAndPeriods)
-    last_half_buffer = (uint16_t *) buffer;
-
-  last_num = num;
+  (void) num;
+  (void) buffer;
+  
   cb_count++;
 }
