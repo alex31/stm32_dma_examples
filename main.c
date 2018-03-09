fd8293b9005e532bff0b89766e10f473f3292ed9
Alexandre Bustico <alexandre.bustico@enac.fr>
2018-03-09 14:58:34 +0100

    works, getting both width and period

--- main.c
+++ main.c
@@ -38,25 +38,23 @@
 /* ICU related code                                                          */
 /*===========================================================================*/
 
-#define    ICU_TIM_FREQ         1000000
+#define    ICU_TIM_FREQ         1e6 // 1Mhz
 #define    ICU1_CHANNEL		ICU_CHANNEL_1
-#define    DCR_DBL		(1 << 8) // 2 transfert
+#define    DCR_DBL		((2-1) << 8) // 2 transfert
 // first register to get is CCR1
 #define DCR_DBA			(((uint32_t *) (&ICUD1.tim->CCR) - ((uint32_t *) ICUD1.tim))) 
 
 
 static void error_cb(DMADriver *dmap, dmaerrormask_t err);
-static void end_cb(DMADriver *dmap, void *buffer, const size_t num);
-
 static volatile dmaerrormask_t last_err = 0;
-static volatile size_t	       cb_count = 0;
 
 typedef union {
   struct {
     uint16_t humidity;
     uint16_t temp;
-    uint8_t  chksum;
-  } __attribute__ ((scalar_storage_order ("big-endian"))) ;
+    uint8_t  chksum; 
+  } __attribute__ ((scalar_storage_order ("big-endian"))) ; // DHT halfword are big_endian
+							    // but MCU is little_endian
     uint8_t raw [5];
 }   DhtData;
 
@@ -74,7 +72,7 @@ static const DMAConfig dmaConfig = {
   .inc_memory_addr = true,
   .circular = false,
   .error_cb = &error_cb,
-  .end_cb = &end_cb,
+  .end_cb = NULL,
   .pburst = 0,
   .mburst = 0,
   .fifo = 0
@@ -98,8 +96,8 @@ static const ICUConfig icu1ch1_cfg = {
 static THD_WORKING_AREA(waBlinker, 512);
 static noreturn void blinker (void *arg);
 
-static THD_WORKING_AREA(waSyncDmaTest, 512);
-static noreturn void syncDmaTest (void *arg);
+static THD_WORKING_AREA(waDht22Acquisition, 512);
+static noreturn void dht22Acquisition (void *arg);
 
 int main(void) {
 
@@ -123,7 +121,7 @@ int main(void) {
   icuStart(&ICUD1, &icu1ch1_cfg);
   ICUD1.tim->DCR = DCR_DBL | DCR_DBA;
   icuStartCapture(&ICUD1);
-  chThdCreateStatic(waSyncDmaTest, sizeof(waSyncDmaTest), NORMALPRIO, syncDmaTest, NULL);
+  chThdCreateStatic(waDht22Acquisition, sizeof(waDht22Acquisition), NORMALPRIO, dht22Acquisition, NULL);
   
   consoleLaunch();  
   chThdSleep(TIME_INFINITE); 
@@ -141,71 +139,83 @@ static noreturn void blinker (void *arg)
   }
 }
 
-static noreturn void syncDmaTest (void *arg)
+icucnt_t widthAndPeriods[86] __attribute__((aligned(4))); 
+static noreturn void dht22Acquisition (void *arg)
 {
   (void)arg;
-  chRegSetThreadName("syncDmaTest");
+  chRegSetThreadName("dht22Acquisition");
 
-  DhtData dhtData;
-  icucnt_t widthAndPeriods[84] __attribute__((aligned(4))) = {0xff}; // took 0.0128 seconds to fill
+  static DhtData dhtData;
 
+  // use bitbanding capability of STM32F4 (see ref manuel RM090)
+  volatile uint32_t * const fhtRegBB = bb_sramp(&dhtData, 0);
 
+  // when the pin is in gpio mode, should be forced LOW
+  palClearLine(LINE_A08_ICU_IN);
+  
   while (true) {
-    palSetLineMode(LINE_A08_ICU_IN, PAL_MODE_OUTPUT_PUSHPULL);
-    palClearLine(LINE_A08_ICU_IN);
+
+    // do the interrogation pulse on the pin
+    palSetLineMode(LINE_A08_ICU_IN, PAL_MODE_OUTPUT_OPENDRAIN);
     chThdSleepMicroseconds(MCU_REQUEST_WIDTH);
-    palSetLine(LINE_A08_ICU_IN);
-    
-    
-    /*
-     * Initializes the ICU driver 1.
-     * GPIOA8 is the ICU input.
-     */
+
+    // revert pin to input capture mode
     palSetLineMode(LINE_A08_ICU_IN, PAL_MODE_ALTERNATE(AF_LINE_A08_ICU_IN)
 		   | PAL_STM32_PUPDR_PULLUP);
-    chThdSleepMilliseconds(5);
 
-    cb_count =0;
+    // launch a DMA transfert from timer in input capure mode and memory
     msg_t status = dmaTransfertTimeout(&dmap, &TIM1->DMAR, widthAndPeriods, ARRAY_LEN(widthAndPeriods),
-				       TIME_MS2I(1000));
+				       TIME_MS2I(20));
+
     if (status != MSG_OK) {
       DebugTrace ("DMA Time OUT");
     }
-
-    DebugTrace ("cb_count = %d", cb_count);
-    //chprintf(chp, "\033[2J\033[1;1H");
-
-    // decodage trame binaire DHT
-
+   if (last_err != MSG_OK) {
+      DebugTrace ("DMA Error");
+      last_err = 0;
+    }
+     
+    
+    // generate binary stream using pulse width registered by ICU+DMA
     uint8_t bit_counter=0;
-    volatile uint32_t * const fhtRegBB = bb_sramp(&dhtData, 0);
+
     for (size_t i=0; i<ARRAY_LEN(widthAndPeriods)/2; i++) {
-      const icucnt_t width = widthAndPeriods[1+(bit_counter*2)];
-      DebugTrace ("width[%u] = %lu", bit_counter, width);
+      const icucnt_t width = widthAndPeriods[1+(i*2)];
+      //      DebugTrace ("width[%u] = %lu bc=%u", i, width, bit_counter);
       if (width >= DHT_START_BIT_WIDTH)
 	/* starting bit resetting the bit counter */
 	bit_counter = 0;
       else 
 	fhtRegBB[bit_counter++] = width >= DHT_INTER_BIT_WIDTH;
+
+      // if frame is completely acquired, exit loop 
+      if (bit_counter == 40)
+	break;
     }
+  
+  
+    // decode binary stream and generate numeric field
+
+    // DHT most significant bit is bit0, but MCU  most significant bit is bit7
+    // revbit reverse the bits order in a byte/halfword,word
+    for (uint i=0; i< sizeof(dhtData.raw); i++) {
+      dhtData.raw[i] = revbit(dhtData.raw[i]);
+    }
+    
+    // compute and compare checksum
+    if (dhtData.raw[4] == ((dhtData.raw[0] + dhtData.raw[1] +
+			    dhtData.raw[2] + dhtData.raw[3]) & 0xff)) {
+
+      const float humidity = dhtData.humidity / 10.0f;
+      const float temp = (dhtData.temp & 0x7fff) / ((dhtData.temp & 0x8000) ? - 10.0f : 10.0f);
+      chprintf(chp, "Temperature: %.2f C, Humidity Rate: %.2f %% \n\r",
+	       temp, humidity);
+    } else {
+      chprintf(chp, "Checksum FAILED!\n\r");
+    }
+    
+    chThdSleepMilliseconds(1000);
   }
-  
-  // decodage champs DHT
-  for (uint i=0; i< sizeof(dhtData.raw); i++) {
-    dhtData.raw[i] = revbit(dhtData.raw[i]);
-  }
-  
-  const float humidity = dhtData.humidity / 10.0f;
-  const float temp = (dhtData.temp & 0x7fff) / ((dhtData.temp & 0x8000) ? - 10.0f : 10.0f);
-  
-  if (dhtData.raw[4] == ((dhtData.raw[0]+dhtData.raw[1]+dhtData.raw[2]+dhtData.raw[3]) & 0xff)) {
-    chprintf(chp, "Temperature: %.2f C, Humidity Rate: %.2f %% \n\r",
-	     temp, humidity);
-  } else {
-    chprintf(chp, "Checksum FAILED!\n\r");
-  }
-  
-  chThdSleepMilliseconds(1000);
 }
 
 
@@ -214,14 +224,6 @@ static noreturn void syncDmaTest (void *arg)
 static void error_cb(DMADriver *_dmap, dmaerrormask_t err)
 {
   (void) _dmap;
-  last_err = err;
+  last_err |= err;
 }
 
-static void end_cb(DMADriver *_dmap, void *buffer, size_t num)
-{
-  (void) _dmap;
-  (void) num;
-  (void) buffer;
-  
-  cb_count++;
-}
