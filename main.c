dd42996b23d6cb8f8a151bf140d939d0ad98051b
Alexandre Bustico <alexandre.bustico@enac.fr>
2018-04-17 17:14:54 +0200

    generation of dshot frame seems to work (tested only with logic analyser)

--- main.c
+++ main.c
@@ -23,20 +23,53 @@
 
 */
 
-#define WIDTHS_SIZE	 256
-#define CHANNELS	 4
-#define PWM_FREQ         125
-#define TICKS_PER_PERIOD 22400 // we can also use 1e4 notation
+#define DSHOT_SPEED_KHZ	 150
 
+#define DSHOT_BIT_WIDTHS 16
+#define DSHOT_FRAME_SILENT_SYNC_BITS 4
+#define DSHOT_DMA_BUFFER_SIZE	 (DSHOT_BIT_WIDTHS+DSHOT_FRAME_SILENT_SYNC_BITS)
+#define CHANNELS	 4
+#define PWM_FREQ         84000 // the timer will beat @84Mhz
+#define TICKS_PER_PERIOD 1000  // that let use any timer :
+			       // does not care if linked to PCLK1 or PCLK2
+			       // tick_per_period will be dynamically calculated 
+			       // after pwm init
+#define DSHOT_SPEED (DSHOT_SPEED_KHZ*1000)
 #define TICK_FREQ (PWM_FREQ * TICKS_PER_PERIOD)
+#define DSHOT_PWM_PERIOD (TICK_FREQ/DSHOT_SPEED)
+#define DSHOT_BIT0_DUTY (DSHOT_PWM_PERIOD * 373 / 1000)
+#define DSHOT_BIT1_DUTY (DSHOT_BIT0_DUTY*2)
 #define    DCR_DBL              ((4-1) << 8) // 2 transfert
 // first register to get is CCR1
 #define DCR_DBA                 (((uint32_t *) (&PWMD1.tim->CCR) - ((uint32_t *) PWMD1.tim))) 
 
-typedef uint16_t timer_reg_t;
-timer_reg_t widths[WIDTHS_SIZE][CHANNELS] __attribute__((aligned(16))); 
 
- 
+typedef uint16_t timer_reg_t;
+
+typedef union {
+  struct {
+    uint16_t throttle:11;
+    uint16_t telemetryRequest:1;
+    uint16_t crc:4;
+  };
+  uint16_t rawFrame;
+}  DshotPacket;
+
+typedef struct {
+  DshotPacket dp[CHANNELS];
+} DshotPackets;
+
+typedef struct {
+  timer_reg_t widths[DSHOT_DMA_BUFFER_SIZE][CHANNELS] __attribute__((aligned(16)));
+} DshotDmaBuffer;
+
+DshotPackets dshotMotors;
+
+
+DshotPacket makeDshotPacket(const uint16_t throttle, bool tlmRequest);
+void        buildDshotDmaBuffer(const DshotPackets * const dsp,  DshotDmaBuffer * const dma);
+
+
 static const DMAConfig dmaConfig = {
   .stream = STM32_PWM1_UP_DMA_STREAM,
   .channel = STM32_PWM1_UP_DMA_CHANNEL,
@@ -47,7 +80,7 @@ static const DMAConfig dmaConfig = {
   .msize = sizeof(timer_reg_t), // type of width array
   .inc_peripheral_addr = false,
   .inc_memory_addr = true,
-  .circular = true,
+  .circular = false,
   .error_cb = NULL,
   .end_cb = NULL,
   .pburst = 0,
@@ -58,14 +91,11 @@ static const DMAConfig dmaConfig = {
 static DMADriver dmap;
 
 
-// pour les parties 1 (persistance) et 2 (servo) et 3 (il variation continue du pwm)
-// n'y a pas besoin de modifier  la structure de configuration.
-// Il n'y a que dans la partie Utilisation des 4 canaux
-// que le champ .channels de cette structure devra être modifié
-static PWMConfig pwmcfg = {     // pwm d'une frequence d'un hz et 10000 pas de quantification
-  .frequency = TICK_FREQ,        // TickFreq : PwmFreq(1) * ticksPerPeriod(10000)  
-  .period    = TICKS_PER_PERIOD, //   tickPerPeriod (10000)
-  .callback  = NULL,             //   pas de callback de passage à l'etat actif
+
+static PWMConfig pwmcfg = {     
+  .frequency = TICK_FREQ,       
+  .period    = TICKS_PER_PERIOD,
+  .callback  = NULL,            
   .channels  = {
     {.mode = PWM_OUTPUT_ACTIVE_HIGH, .callback = NULL},
     {.mode = PWM_OUTPUT_ACTIVE_HIGH, .callback = NULL},
@@ -80,8 +110,6 @@ static PWMConfig pwmcfg = {     // pwm d'une frequence d'un hz et 10000 pas de q
 
 static THD_WORKING_AREA(waBlinker, 512);
 static noreturn void blinker (void *arg);
-
-const float pi=3.15169f;
 
 int main(void) {
 
@@ -98,29 +126,35 @@ int main(void) {
   initHeap();
   dmaObjectInit(&dmap);
 
-
-  for (size_t i=0; i< WIDTHS_SIZE; i++) {
-    const float angle = i*pi/WIDTHS_SIZE;
-    for(size_t j=0;j<4;j++) {
-      widths[i][j] = TICKS_PER_PERIOD * powf(sinf(angle+(j*pi/4)), 10.0f);
-    }
-  }
-  
   consoleInit();
 
   chThdCreateStatic(waBlinker, sizeof(waBlinker), NORMALPRIO, blinker, NULL);
   dmaStart(&dmap, &dmaConfig);
   pwmStart(&PWMD1, &pwmcfg);
-  PWMD1.tim->DCR = DCR_DBL | DCR_DBA;
+  PWMD1.tim->DCR = DCR_DBL | DCR_DBA; // enable bloc register DMA transaction
+  pwmChangePeriod(&PWMD1, DSHOT_PWM_PERIOD);
 
   for (size_t j=0;j<4;j++)
     pwmEnableChannel(&PWMD1, j, 1);
   
-  dmaStartTransfert(&dmap, &TIM1->DMAR, widths, WIDTHS_SIZE*CHANNELS);
+  consoleLaunch();
 
+  dshotMotors.dp[0] = makeDshotPacket(1025,0);
+  dshotMotors.dp[1] = makeDshotPacket(514,0);
+  dshotMotors.dp[2] = makeDshotPacket(260,1);
+  dshotMotors.dp[3] = makeDshotPacket(1046,0);
   
-  consoleLaunch();  
-  chThdSleep(TIME_INFINITE); 
+  DshotDmaBuffer dsdb;
+  buildDshotDmaBuffer(&dshotMotors, &dsdb);
+  
+  while (true) {
+    if (dmap.state == DMA_READY) {
+      dmaStartTransfert(&dmap, &TIM1->DMAR, &dsdb, DSHOT_DMA_BUFFER_SIZE * CHANNELS);
+    } else {
+      DebugTrace ("DMA Not Ready");
+    }
+    chThdSleepMilliseconds(1);
+  }
 }
 
 
@@ -135,3 +169,31 @@ static noreturn void blinker (void *arg)
   }
 }
 
+DshotPacket makeDshotPacket(const uint16_t throttle, bool tlmRequest)
+{
+  DshotPacket dp = {.throttle = throttle,
+		    .telemetryRequest =  (tlmRequest ? 1 : 0),
+		    .crc = 0};
+  
+  // compute checksum
+  uint16_t csum = (throttle << 1) | (tlmRequest ? 1 : 0);
+  for (int i = 0; i < 3; i++) {
+    dp.crc ^=  csum;   // xor data by nibbles
+    csum >>= 4;
+  }
+  
+  return dp;
+}
+
+void buildDshotDmaBuffer(const DshotPackets * const dsp,  DshotDmaBuffer * const dma)
+{
+  for (size_t chanIdx=0; chanIdx < CHANNELS; chanIdx++) {
+    for (size_t bitIdx=0; bitIdx < DSHOT_BIT_WIDTHS; bitIdx++) {
+      dma->widths[bitIdx][chanIdx] = dsp->dp[chanIdx].rawFrame & (1 << bitIdx) ?
+	DSHOT_BIT1_DUTY : DSHOT_BIT0_DUTY;
+    }
+    for (size_t bitIdx=DSHOT_BIT_WIDTHS; bitIdx < DSHOT_DMA_BUFFER_SIZE; bitIdx++) {
+      dma->widths[bitIdx][chanIdx] = 0;
+    }
+  }
+}
